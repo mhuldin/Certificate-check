82d625c3fd0d14c6a30d70ab3837db5def0674a8
mhuldin <huldin@gmail.com>
2014-01-13 13:53:42 +0100

    Update find_cert_and_list.sh
    Removed all keytool every time and exporting data to a text file to work with.

--- find_cert_and_list.sh
+++ find_cert_and_list.sh
@@ -1,14 +1,11 @@
 #!/bin/bash/
-PASSWORD="--storepass <password>"
-#KEYSTORE="cacerts"
 THRESHOLD_IN_DAYS="30"
 KEYTOOL="keytool"
-
-KEYSTORE=$(find / -name cacerts)
-#KEYSTORE=$(find / -name *.jks)
-
+KEYSTORE=$(find / \( -name "*.jks" -o -name "cacerts" \) | grep -iv java)
 for i in $KEYSTORE
 do
+        echo "\n" | keytool -list -v -keystore "$i" > /tmp/certi
+
         CURRENT=`date +%s`
         THRESHOLD=$(($CURRENT + ($THRESHOLD_IN_DAYS*24*60*60)))
         if [ $THRESHOLD -le $CURRENT ]; then
@@ -17,24 +14,23 @@ do
         fi
         echo "Looking for certificates inside the keystore $i expiring in $THRESHOLD_IN_DAYS day(s)..."
 
-        $KEYTOOL -list -v -keystore "$i"  $PASSWORD 2>&1 > /dev/null
-        #if [ $? -gt 0 ]; then echo "Error opening the keystore."; exit 1; fi
         if [ $? -gt 0 ]; then echo "Error opening the keystore."; fi
 
-        $KEYTOOL -list -v -keystore "$i" $PASSWORD | grep Alias | awk '{print $3}' | while read ALIAS
+        cat /tmp/certi | grep "Alias" | awk '{print $3}' | while read Alias
         do
-                #Iterate through all the certificate alias
-                EXPIRACY=`$KEYTOOL -list -v -keystore "$i"  $PASSWORD -alias $ALIAS | grep Valid`
-                UNTIL=`$KEYTOOL -list -v -keystore "$i"  $PASSWORD -alias $ALIAS | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
+                EXPIRACY=`cat /tmp/certi | grep -A20 "$Alias" | grep Valid`
+                UNTIL=`cat /tmp/certi | grep -A20 "$Alias" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
                 UNTIL_SECONDS=`date -d "$UNTIL" +%s`
-                REMAINING_DAYS=$(( ($UNTIL_SECONDS -  $(date +%s)) / 60 / 60 / 24 ))
+                REMAINING_DAYS=$(( ($UNTIL_SECONDS - $(date +%s)) / 60 / 60 / 24 ))
                 if [ $THRESHOLD -le $UNTIL_SECONDS ]; then
-                        echo "[OK]      Certificate $ALIAS expires in '$UNTIL' ($REMAINING_DAYS day(s) remaining)."
+                echo "[OK]      Certificate with Alias $Alias
+                expires in '$UNTIL' ($REMAINING_DAYS day(s) remaining)."
                 else
-                        echo "[WARNING] Certificate $ALIAS expires in '$UNTIL' ($REMAINING_DAYS day(s) remaining)."
-                        RET=1
+                echo "[WARNING]     Certificate with Alias $Alias"
+                echo "         expires in '$UNTIL' ($REMAINING_DAYS day(s) remaining)."
+                echo "$i with $Alias expires $UNTIL" >> /tmp/warnings
+                RET=1
                 fi
-
 done
 done
-        exit $RET
+exit $RET
